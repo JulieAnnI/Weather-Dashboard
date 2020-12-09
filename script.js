abb329522a7e51f0d889fec6ef9c2564e086148a
Julie Ann <julieanniwinski@gmail.com>
2020-12-09 12:54:24 -0500

    Started script with AJAX call

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 
+var cities = [];
+
+function displayWeatherInfo() {
+    var city = $(this).attr("data-name");
+    var ApiKey = "d039ab187b34696aa254cc930a98ecb6";
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+
+        var cityDiv = $("<div class='city'>");
+        var 
+
+    })
+    
+
+
+}
+
+
+function renderButtons() {
+
+
+}
+
+
+$(".saveBtn").on("click", function(event){
+   event.preventDefault();
+   
+   var city = $(".citysearch").val().trim();
+
+   cities.push(city);
+
+   renderButtons();
+
+});
