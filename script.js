ee1a375579a631d16ad2a1c7993a07b53848e61d
Julie Ann <julieanniwinski@gmail.com>
2020-12-10 23:29:03 -0500

    Added local Storage for cities

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
 
 var cities = [];
+var ApiKey = "d039ab187b34696aa254cc930a98ecb6";
 
-function displayWeatherInfo() {
-    var city = $(this).attr("data-name");
-    var ApiKey = "d039ab187b34696aa254cc930a98ecb6";
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`;
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-
-        var cityDiv = $("<div class='city'>");
-        var 
-
-    })
-    
-
-
-}
 
 
 function renderButtons() {
@@ -29,11 +12,44 @@ function renderButtons() {
 
 $(".saveBtn").on("click", function(event){
    event.preventDefault();
-   
-   var city = $(".citysearch").val().trim();
 
-   cities.push(city);
+   var city = $("#city-input").val().trim();
 
-   renderButtons();
+   if (city === "") {
+       alert("Please enter valid city");
+    
+   } else {
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`;
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            cityName = response.name;
+        
+        cities.push(cityName);
+        localStorage.setItem("cities", JSON.stringify(cities))
+        console.log(localStorage);
+        })
+        
+    }
 
 });
+
+
+function displayCurrentWeather() {
+    var city = $(this).attr("data-name");
+    
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+
+        const tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        $("#city").html(`${response.name}`);
+    })
+}
